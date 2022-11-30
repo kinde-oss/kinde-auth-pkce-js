ef7c32b4ea4fd0e13c04a43e813ea118fec730f3
Dave Berner <davidajberner@gmail.com>
2022-12-01 00:30:25 +1100

    fix up build

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,26 @@
+import {createRequire} from 'node:module';
+const require = createRequire(import.meta.url);
+const pkg = require('./package.json');
 import terser from '@rollup/plugin-terser';
 import dts from 'rollup-plugin-dts';
 
 export default [
   {
     input: 'src/main.js',
-
-    plugins: [terser()],
     output: [
       {
         name: 'createKindeClient',
-        file: 'dist/kinde-auth-pkce-js.umd.min.js',
-        format: 'umd'
+        file: pkg.main,
+        format: 'umd',
+        plugins: [terser()]
       },
       {
-        file: 'dist/kinde-auth-pkce-js.esm.min.js',
+        file: pkg.module,
         format: 'es'
       }
     ]
   },
   {
-    // path to your declaration files root
     input: 'index.d.ts',
     output: [{file: 'dist/index.d.ts', format: 'es'}],
     plugins: [dts()]
