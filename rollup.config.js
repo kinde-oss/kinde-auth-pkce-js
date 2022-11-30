775148afe92cea8454d957918f2c52e086dc6475
Dave Berner <davidajberner@gmail.com>
2022-11-30 21:00:31 +1100

    update rollup and make types public

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,27 @@
-import {terser} from 'rollup-plugin-terser';
-import pkg from './package.json';
+import terser from '@rollup/plugin-terser';
+import dts from 'rollup-plugin-dts';
 
-export default {
-  input: 'src/main.js',
+export default [
+  {
+    input: 'src/main.js',
 
-  plugins: [terser()],
-  output: [
-    {
-      name: 'createKindeClient',
-      file: pkg.browser,
-      format: 'umd'
-    },
-    {
-      file: pkg.module,
-      format: 'es'
-    }
-  ]
-};
+    plugins: [terser()],
+    output: [
+      {
+        name: 'createKindeClient',
+        file: 'dist/kinde-auth-pkce-js.umd.min.js',
+        format: 'umd'
+      },
+      {
+        file: 'dist/kinde-auth-pkce-js.esm.min.js',
+        format: 'es'
+      }
+    ]
+  },
+  {
+    // path to your declaration files root
+    input: 'index.d.ts',
+    output: [{file: 'dist/index.d.ts', format: 'es'}],
+    plugins: [dts()]
+  }
+];
